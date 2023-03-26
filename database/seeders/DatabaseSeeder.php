8edadb6c1d69f077a69f40b1d3b4da890c03e41f
veansyah91 <49179821+veansyah91@users.noreply.github.com>
2023-03-26 10:25:30 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             ContactSeeder::class,
             SubClassificationAccountSeeder::class,
             AccountSeeder::class,
+            SubscribeSeeder::class,
         ]);
     }
 }
